0cbdd34e9b9878103a63c6fc1a5d65451b012c75
ferrarenCJ <ferrarecliff@gmail.com>
2026-08-17 22:05:48 -0700

    Added the solution for the Final Assignment 12.1

--- 05_Assignment/Required_Final_Assignment_12.1/Part 3/read.py
+++ 05_Assignment/Required_Final_Assignment_12.1/Part 3/read.py
@@ -2,8 +2,13 @@
 import redis
 
 # connect to server
-r = redis.Redis(host='localhost', port=6379, db=0)
+r = redis.Redis(
+    host='localhost',
+    port=6379,
+    db=0,
+    decode_responses=True
+)
 
-# read items
-for item in r.lrange('nums', 0, -1):
-    print(item)
+# read values
+print("Milk:", r.get("Milk"))
+print("Bread:", r.get("Bread"))
--- 05_Assignment/Required_Final_Assignment_12.1/Part 3/write.py
+++ 05_Assignment/Required_Final_Assignment_12.1/Part 3/write.py
@@ -1,10 +1,15 @@
 # pip install redis
 import redis
-from datetime import datetime
 
-# connect to server
-r = redis.Redis(host='localhost', port=6379, db=0)
+r = redis.Redis(
+    host='localhost',
+    port=6379,
+    decode_responses=True
+)
 
-# write to server
-t = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-r.rpush('nums',t)
+r.mset({
+    "Milk": "Lactose",
+    "Bread": "Gluten"
+})
+
+print("Values written successfully.")
